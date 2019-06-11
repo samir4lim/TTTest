d74ced5dbe34242c52f8b1a99d31a05bbb26f053
samir <samir4lim@gmail.com>
2019-06-11 10:52:56 +0800

    Add Tip Button and Label

--- JTestDemo/ViewController.m
+++ JTestDemo/ViewController.m
@@ -9,6 +9,7 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *testLabel;
 
 @end
 
@@ -19,5 +20,13 @@
     // Do any additional setup after loading the view.
 }
 
+- (IBAction)testOoopsAction:(id)sender {
+    NSInteger randomNumber = ((arc4random()%2) ? 1 : 0);
+    NSString *testText = @"Simple Test hhhh01";
+    if (randomNumber) {
+        testText = @"Simple Test randomNumber";
+    }
+    _testLabel.text = testText;
+}
 
 @end
